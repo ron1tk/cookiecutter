fd032655b7aea80c50901502b029328891475b83
Ronit Kapoor <ronitkapoor@Ronits-MacBook-Air-3.local>
2024-12-12 09:51:46 -0800

    new

--- cookiecutter/cli.py
+++ cookiecutter/cli.py
@@ -65,6 +65,10 @@ def validate_extra_context(
 
 
 
+
+
+
+
                 f"EXTRA_CONTEXT should contain items of the form key=value; "
                 f"'{string}' doesn't match that form"
             )
